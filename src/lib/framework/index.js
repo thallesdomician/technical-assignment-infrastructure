db89f1671438c3a1c547008b1660ce4d8027a959
Thalles Oliveira <thallesdomician@gmail.com>
2023-09-29 01:20:30 +1000

    feat: middleware handler web app class

--- src/lib/framework/index.js
+++ src/lib/framework/index.js
@@ -14,15 +14,38 @@ class WebApp {
     const server = http.createServer((req, res) => {
       this.handleMiddleware(req, res);
     });
-  
+
     server.listen(port, () => {
       console.log(`Server is running on port ${port}`);
       if (callback) {
         callback(server);
       }
     });
-  
+
     return server;
+  }
+
+  handleMiddleware(req, res) {
+    let index = 0;
+    let responseEnded = false;
+
+    const next = async () => {
+      const middleware = this.middlewares[index++];
+     
+      await middleware(req, res, next);
+      if(responseEnded) return; 
+      if (res.body) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        const content = JSON.stringify(res.body);
+        res.end(content);
+      } else {
+        res.end();
+      }
+      responseEnded = true;
+
+    };
+
+    next();
   }
 }
 
