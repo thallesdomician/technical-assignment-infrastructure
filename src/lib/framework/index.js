8f1441a22420a3abe223d30a55f478fea621ed09
Thalles Oliveira <thallesdomician@gmail.com>
2023-09-28 14:10:25 +1000

    feat: implement server start

--- src/lib/framework/index.js
+++ src/lib/framework/index.js
@@ -8,6 +8,22 @@ class WebApp {
   use(middleware) {
     this.middlewares.push(middleware);
   }
+
+  start(port, callback) {
+    const http = require('http');
+    const server = http.createServer((req, res) => {
+      this.handleMiddleware(req, res);
+    });
+  
+    server.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+      if (callback) {
+        callback(server);
+      }
+    });
+  
+    return server;
+  }
 }
 
 module.exports = WebApp;
