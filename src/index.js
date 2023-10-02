7621279af48e9616d118699ca82d57e4caf80106
Thalles Oliveira <thallesdomician@gmail.com>
2023-10-03 01:18:53 +1100

    feat: index changes suggestions

--- src/index.js
+++ src/index.js
@@ -7,16 +7,22 @@
  */
 
 const WebApp = require('./lib/framework');
+const { parseRequestJsonMiddleware, parseResponseJsonMiddleware } = require('./lib/middleware');
 const auth = require('./middleware/auth');
 const documentRoutes = require('./routes/documentRoutes');
 
 // TODO: implement a constructor [1]
 const app = new WebApp();
 
+// middleware parse request and response json is provided out-of-th box by the webapp lib, although each developer should decide when it will used
+// app.use(parseRequestJsonMiddleware);
+
 // TODO: implement the 'use' method which registers the global application middleware (ie. auth middleware, routers, etc.) [2]
 app.use(auth);
 // TODO: router should implement the 'routes' method which builds the routing middleware [9]
 app.use(documentRoutes.routes({ prefix: '/documents' }));
+// app.use(parseResponseJsonMiddleware);
+
 
 if (process.env.NODE_ENV === 'test') {
   module.exports = app;
